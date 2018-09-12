bc35ac6d00bc41871e8dc13682e71f52569f42cb
Geoffrey Arnold <geoffrey.arnold@pittsburghpa.gov>
2018-09-12 10:46:03 -0400

    Meeting examples

--- hw_1_afierro_taketwo.R
+++ hw_1_afierro_taketwo.R
@@ -18,38 +18,29 @@ arrests$UrbanPop <- NULL
 
 #Create new variable with state names
 # In the future would be a great reason to use the dplyr function add_rownames() but this works too, no deduction, just a tip!
-arrests$State <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas" ,"Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota","Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming")
+arrests <- arrests %>%
+  tibble::rownames_to_column(var = "State")
 
 #Get dataset for shiny table
 PlotArrests <- data.frame(arrests)
 PlotArrests$State <- NULL
 
 #Subset data top 5 most populated states
-MostPop <- subset (arrests, subset = 
-                     (arrests$State == "California") |
-                     (arrests$State == "Texas") |
-                     (arrests$State == "Florida") |
-                     (arrests$State == "New York") |
-                     (arrests$State == "Pennsylvania")
-)
+MostPop <- subset (arrests, State %in% c("California", "Texas", "Florida", "New York", "Pennsylvania"))
 
 #Melt data to put it in long form so I can use it to fill by variable in ggplot
 USarrests <- melt(MostPop, id.vars = "State")
 
 #Capitalize column names because it's bothering me not to do that.
 # This bugs me too! You can use toTitleCase from the tools package in the future
-names(USarrests)[2]<-"Variable" # example: tools::names(USarrests) <- toTitleCase(names(USarrests))
-names(USarrests)[3]<-"Value"
+names(USarrests) <- tools::toTitleCase(names(USarrests))
 
 pdf(NULL)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
   titlePanel("USA Arrests in 1973 Grid"),
-  
-  sidebarLayout(
-    sidebarPanel(
+  inputPanel(
       helpText("Explore Arrests by Crime in the 5 Most Populous States"),
       
       selectInput("Var",
@@ -57,27 +48,27 @@ ui <- fluidPage(
                   c("Murder", 
                     "Assault",
                     "Rape"),
-                  selected = "Murder")
-),
-    
-    mainPanel(
+                  selected = "Murder"),
+      fileInput("file", h3("File input"))
+      ),
+    fluidPage(
       # You don't have anything in the server function creating this output
       textOutput("selected_var"),
-      
-      fluidRow(column(3,
-                      # I would have probably put this in the sidebar panel, but its a subjective design choice, no pts removed.
-                      fileInput("file", h3("File input")))),
-      # You shouldn't state the mainPanel function, you can just make another row like you did for the Data Table.
-      mainPanel(
+      fluidRow(
         plotlyOutput("plot")
-),
+        ),
       fluidRow(column(12,
                       DT::dataTableOutput("table")
+                      )
+               )
+      )
 )
-))))
 
 server <- function(input, output) {
-  
+  # Rendering Text
+  output$selected_var <- renderText({
+    paste("You have selected the following variables:", input$Var)
+  })
   output$plot <- renderPlotly({
     ggplot(data = USarrests, aes(x = State, y = Value, fill = Variable)) + geom_bar(stat = "identity")
   })  
